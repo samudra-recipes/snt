60985444bc46398d09c47d7a059526a2e2a2bc47
samudragogoi <50080120+samudragogoi@users.noreply.github.com>
2023-04-04 07:20:17 +0530

    f

---
+++ utils.js
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+window.gltfLoader = new THREE.GLTFLoader();
+/**
+ * The Reticle class creates an object that repeatedly calls
+ * `xrSession.requestHitTest()` to render a ring along a found
+ * horizontal surface.
+ */
+class Reticle extends THREE.Object3D {
+  constructor() {
+    super();
+
+    this.loader = new THREE.GLTFLoader();
+    this.loader.load("shantiniketan.glb", (glb) => {
+      this.add(glb.scene);
+    })
+
+    this.visible = false;
+  }
+}
+
+
+window.DemoUtils = {
+  /**
+   * Creates a THREE.Scene containing lights that case shadows,
+   * and a mesh that will receive shadows.
+   *
+   * @return {THREE.Scene}
+   */
+  createLitScene() {
+    const scene = new THREE.Scene();
+    const light = new THREE.AmbientLight(0xffffff, 1);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
+    directionalLight.position.set(10, 15, 10);
+    directionalLight.castShadow = true;
+
+    const planeGeometry = new THREE.PlaneGeometry(2000, 2000);
+    planeGeometry.rotateX(-Math.PI / 2);
+
+    const shadowMesh = new THREE.Mesh(planeGeometry, new THREE.ShadowMaterial({
+      color: 0x111111,
+      opacity: 0.2,
+    }));
+
+    shadowMesh.name = 'shadowMesh';
+    shadowMesh.receiveShadow = true;
+    shadowMesh.position.y = 10000;
+    scene.add(shadowMesh);
+    scene.add(light);
+    scene.add(directionalLight);
+
+    return scene;
+  },
+
+  /**
+   * Creates a THREE.Scene containing cubes all over the scene.
+   *
+   * @return {THREE.Scene}
+   */
+};
+
+/**
+ * Toggle on a class on the page to disable the "Enter AR"
+ * button and display the unsupported browser message.
+ */
+function onNoXRDevice() {
+  document.body.classList.add('unsupported');
+}
